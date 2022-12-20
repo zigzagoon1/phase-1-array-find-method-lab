8594c87ceb122b67564daad486b89f2301f7c584
KellyMark <kmark602@gmail.com>
2022-12-20 13:55:41 -0500

    commit tree 9232467aa967c077d5d62781956d8ad073427980

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+array = [{
+    year: 2018,
+    result: win
+}, {
+    year: 2019, 
+    result: lose
+}]
+
+function superbowlWin(arrayOfObjects) {
+    const match = arrayOfObjects.find(x => x.result === "W");
+    if (match !== null && match !== undefined) {
+        return match.year;
+    }
+    else {
+        return undefined;
+    }
+}
+
